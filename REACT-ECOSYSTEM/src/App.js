7a20e6aa0053dbd7b59b409722488144efffcea9
Sarita Mandal <mandalsarita11@gmail.com>
2021-07-02 16:06:59 +0530

    adding react ecosystem

---
+++ REACT-ECOSYSTEM/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <h1>hello react from scratch</h1>
+  </div>
+);
+
+export default App;
---
+++ REACT-ECOSYSTEM/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude:
+      }
+    ]
+  }
+}
